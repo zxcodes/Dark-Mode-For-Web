5bf754cbd2e8f1c833609807009799f2a1fb649b
Mohammed Farmaan <farmaanghouse6@gmail.com>
2021-04-13 07:00:37 +0530

    added comments and made minor style changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
+// Function that swaps the stylesheet.
 function changeTheme() {
   let theme = document.getElementById("theme");
   let lightTheme = "styles/light-mode.css";
   let darkTheme = "styles/dark-mode.css";
-
+  // Checking what stylesheet the link tag has.
   if (theme.getAttribute("href") == lightTheme) {
     theme.href = darkTheme;
   } else {
